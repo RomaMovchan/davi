810c5151d6f3b293ae23cdca454f094d3d2a8b1a
Roma Movchan <romamovchan123@gmail.com>
2017-07-24 15:52:33 +0300

    add reg styles

--- Davi/src/app/auth/auth.component.ts
+++ Davi/src/app/auth/auth.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { UserService } from '../shared/services/user.service';
+import { HttpService } from '../shared/services/http.service';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  authForm: FormGroup;
+  error = false;
 
-  constructor() { }
+  constructor(private httpService: HttpService,
+              private userService: UserService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private fb: FormBuilder
+  ) {
+    this.authForm = fb.group({
+      'username': new FormControl('', Validators.required),
+      'password': new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit() {
+  }
+
+  public onAuth() {
+
   }
 
 }
--- Davi/src/app/auth/auth.module.ts
+++ Davi/src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ModuleWithProviders} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthComponent } from './auth.component';
 import { AuthGuard } from '../shared/services/auth-guard.service';
@@ -8,8 +9,8 @@ import { AuthGuard } from '../shared/services/auth-guard.service';
 const authRouting: ModuleWithProviders = RouterModule.forChild([
   {
     path: 'login',
-    component: AuthComponent,
-    canActivate: [AuthGuard]
+    component: AuthComponent
+    // canActivate: [AuthGuard]
   },
   {
     path: 'register',
@@ -21,7 +22,9 @@ const authRouting: ModuleWithProviders = RouterModule.forChild([
 @NgModule({
   imports: [
     CommonModule,
-    authRouting
+    authRouting,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [ AuthComponent ]
 })
